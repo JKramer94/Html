ccca07a6e7a4aadbb74f5b9f273478fd38862f00
JoKrH <31029379+JoKrH@users.noreply.github.com>
2017-10-04 10:17:10 +0200

    BUHUHU

---
+++ JavaScript/Button Quiz.js
@@ -0,0 +1,15 @@
+var btn = document.querySelector("button");
+
+// var isPurple = false;
+// btn.addEventListener("click", function() {
+//     if (isPurple) {
+//         document.body.style.backgroundColor = "white";
+//     } else {
+//         document.body.style.backgroundColor = "purple";
+//     }
+//     isPurple = !isPurple;
+// });
+
+btn.addEventListener("click", function() {
+    document.body.classList.toggle("purple")
+});
