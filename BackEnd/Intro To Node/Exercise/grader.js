f5dfea004911a1e49b22258de0d64c76e2fd89c2
JoKrH <31029379+JoKrH@users.noreply.github.com>
2017-10-11 14:41:45 +0200

    opdatering

--- BackEnd/Intro To Node/Exercise/grader.js
+++ BackEnd/Intro To Node/Exercise/grader.js
@@ -6,10 +6,10 @@ function average(score) {
     }
     //Dividere det samlet resultat med længden på det arr der er blevet brugt
     avgNum = total / score.length;
-    console.log(Math.round(avgNum));
+    return Math.round(avgNum);
 }
 //De arrays der sendes til functionen
 var scores1 = [90, 98, 89, 100, 100, 86, 94];
-average(scores1); //should return 94
+console.log(average(scores1)); //should return 94
 var scores2 = [40, 65, 77, 82, 80, 54, 73, 63, 95, 49];
-average(scores2); //should return 68
+console.log(average(scores2)); //should return 68
