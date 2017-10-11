7653db3020a71aa4d4c490c75e97c7dead3dea82
JoKrH <31029379+JoKrH@users.noreply.github.com>
2017-10-11 14:40:10 +0200

    s

--- BackEnd/Intro To Node/Exercise/grader.js
+++ BackEnd/Intro To Node/Exercise/grader.js
@@ -1,15 +1,15 @@
 //Finder gemmensnitet af en række tal
-function average(arr) {
+function average(score) {
     var total = 0;
-    for (var i = 0; i < arr.length; i++) {
-        total += arr[i];
+    for (var i = 0; i < score.length; i++) {
+        total += score[i];
     }
     //Dividere det samlet resultat med længden på det arr der er blevet brugt
-    avgNum = total / arr.length;
+    avgNum = total / score.length;
     console.log(Math.round(avgNum));
 }
 //De arrays der sendes til functionen
-var scores = [90, 98, 89, 100, 100, 86, 94];
-average(scores); //should return 94
+var scores1 = [90, 98, 89, 100, 100, 86, 94];
+average(scores1); //should return 94
 var scores2 = [40, 65, 77, 82, 80, 54, 73, 63, 95, 49];
 average(scores2); //should return 68
